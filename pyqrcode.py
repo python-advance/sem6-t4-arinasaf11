676e25e2f86d6c2b4a0e323868cfdf82e5d6eec4
Arina Safiullina <31733496+arinasaf11@users.noreply.github.com>
2019-06-26 11:52:08 +0300

    Update pyqrcode.py

--- pyqrcode.py
+++ pyqrcode.py
@@ -1,6 +1,5 @@
 import pyqrcode
 
 url = pyqrcode.create('https://www.google.com')
-url.svg('uca-url.svg', scale=2, module_color='#177d00')
-url.eps('uca-url.eps', scale=2, module_color='#277834')
+url.svg('uca-url.svg', scale=2, module_color='#177d00') #зеленый цвет
 print(url.terminal(quiet_zone=1))
